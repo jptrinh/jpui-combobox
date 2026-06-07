35358731cf8b1e8a142523a2ac28930eef191fdc
jptrinh <hello@jptrinh.com>
2026-06-07 10:20:15 +0200

    Change chipGap type back to 'Length' and update configuration options

--- ww-config.js
+++ ww-config.js
@@ -1039,7 +1039,7 @@ export default {
         },
         chipGap: {
             label: { en: 'Gap' },
-            type: 'Spacing',
+            type: 'Length',
             section: 'style',
             options: {
                 unitChoices: [
@@ -1047,7 +1047,7 @@ export default {
                     { value: 'rem', label: 'rem', min: 0, max: 2 },
                 ],
                 noRange: true,
-                isCorner: false,
+                useVar: true,
             },
             bindable: true,
             responsive: true,
